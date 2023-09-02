6184a9f85abcd7a12d68cab34202c48c9fedbca5
user <user@MacBook-Pro-user.local>
2023-09-02 17:10:20 +0300

    structure and first steps

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="app-container">
+    <nav class="nav">
+      <router-link to="/about" class="router-link">About Me</router-link>
+      <router-link to="/" class="router-link">Lift Simulator</router-link>
+    </nav>
+    <div class="content-wrapper">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import router from './router'
+
+export default {
+  name: 'App',
+  router
+}
+</script>
+<style>
+/* Общие стили */
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+
+/* Стили для навигационной панели */
+.nav {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  background-color: #4CAF50;
+  padding: 1rem 0;
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.2);
+}
+
+.router-link {
+  font-size: 1.2rem;
+  color: #ffffff;
+  text-decoration: none;
+}
+
+.router-link:hover {
+  text-decoration: underline;
+}
+
+/* Стили для содержимого страницы */
+.content-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+}
+</style>
+
+
+
---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="welcome-container">
+    <h1>Hi, TrueConf team!</h1>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/3/36/Логотип_TrueConf.png"
+      alt="Кортиночка"
+      class="vue-logo"
+    />
+    <p></p>
+    <a href="https://github.com/babymama111" class="github-link" target="_blank" rel="noopener noreferrer">My GitHub Profile</a>
+  </div>
+</template>
+
+<style>
+.welcome-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #000000;
+}
+
+h1 {
+  font-size: 4rem;
+  text-align: center;
+  margin-bottom: 2rem;
+  color: #fffbfb;
+  animation: fadeIn 2s;
+}
+
+.vue-logo {
+  max-width: 30%;
+  height: auto;
+  margin-bottom: 2rem;
+  animation: fadeIn 4s;
+}
+
+p {
+  font-size: 1.5rem;
+  text-align: center;
+  color: #000;
+  font-weight: bold;
+  animation: fadeIn 2s 1s both;
+}
+
+.github-link {
+  font-size: 1.2rem;
+  color: #ffffff;
+  text-decoration: none;
+  margin-top: 1rem;
+  border: 2px solid #ffffff;
+  border-radius: 5px;
+  padding: 0.5rem 1rem;
+  transition: background-color 0.3s ease-in-out;
+  animation: fadeIn 2s 2s both;
+}
+
+.github-link:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+/* Определение анимации fadeIn */
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
